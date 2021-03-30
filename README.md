0143d260ed7fb6743e49f36d639c4a814e25f168
lcao1210 <lcao1992@163.com>
2021-03-30 16:11:49 +0800

    20210330累积更新

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # CTC解码器
-[![Python Version](https://img.shields.io/badge/Python-3.6.9-blue.svg)](https://www.python.org/)
+[![Python Version](https://img.shields.io/badge/Python-3-blue.svg)](https://www.python.org/)
 
 ## 功能介绍
 用于CTC解码，支持贪婪解码(greedy decode)以及束搜索解码(beam search decode)，使用方法详见`demo.py`
@@ -66,4 +66,7 @@ bin/build_binary trie -a 22 -q 8 -b 8 test.arpa test.klm
 ```
 
 ## 感谢
+https://github.com/yeyupiaoling/PaddlePaddle-DeepSpeech
+
+参考版本：
 https://github.com/yeyupiaoling/PaddlePaddle-DeepSpeech/tree/d0ebd704b5b2599e269d1f9852a489ef928e468d
--- demo.py
+++ demo.py
@@ -4,56 +4,50 @@ from decoders.ctcdecoders.swig_wrapper import ctc_greedy_decoder
 
 
 class Model():
-    def __init__(self, alpha, beta, language_model_path, vocab_list,
-                 batch_size, cutoff_prob, cutoff_top_n):
-        self.alpha = alpha
-        self.beta = beta
-        self.language_model_path = language_model_path
-        self.vocab_list = vocab_list
-        self.batch_size = batch_size
-        self.cutoff_prob = cutoff_prob
-        self.cutoff_top_n = cutoff_top_n
-        self.scorer = Scorer(alpha=self.alpha,
-                             beta=self.beta,
-                             language_model_path=self.language_model_path,
-                             vocab_list=self.vocab_list)
+    def __init__(self, vocab_list):
+        self.vocab_list = vocab_list  # 字符集
 
-    def decode_batch_greedy(self, probs_split, vocab_list):
+    def decode_batch_greedy(self, probs_split):
+        # 参数含义参见./decoders/ctcdecoders/swig_wrapper.py
         results = []
         for i, probs in enumerate(probs_split):
-            output_transcription = ctc_greedy_decoder(probs_seq=probs,
-                                                      vocabulary=vocab_list)
+            output_transcription = ctc_greedy_decoder(
+                probs_seq=probs, vocabulary=self.vocab_list)
             results.append(output_transcription)
         return results
 
-    def decode_batch_beam_search(self, probs_split, beam_alpha, beam_beta,
-                                 beam_size, cutoff_prob, cutoff_top_n,
-                                 vocab_list, num_processes):
+    def decode_batch_beam_search(self, probs_split, beam_size, num_processes,
+                                 scorer, cutoff_prob, cutoff_top_n):
+        # 参数含义参见./decoders/ctcdecoders/swig_wrapper.py
         num_processes = min(num_processes, len(probs_split))
         beam_search_results = ctc_beam_search_decoder_batch(
             probs_split=probs_split,
-            vocabulary=vocab_list,
+            vocabulary=self.vocab_list,
             beam_size=beam_size,
             num_processes=num_processes,
-            ext_scoring_func=self._ext_scorer,
+            ext_scoring_func=scorer,
             cutoff_prob=cutoff_prob,
             cutoff_top_n=cutoff_top_n)
-
         results = [result[0][1] for result in beam_search_results]
         return results
 
 
 if __name__ == '__main__':
-    model = Model()
     probs_split = [[0.6, 0.4], [0.2, 0.8], [0.3, 0.7]]
-    result_transcript = model.decode_batch_greedy(probs_split=probs_split,
-                                                  vocab_list=model.vocab_list)
-    result_transcript = model.decode_batch_beam_search(
-        probs_split=probs_split,
-        beam_alpha=model.alpha,
-        beam_beta=model.beta,
-        beam_size=model.beam_size,
-        cutoff_prob=model.cutoff_prob,
-        cutoff_top_n=model.cutoff_top_n,
-        vocab_list=model.vocab_list,
-        num_processes=16)
+    vocab_list = ['_', 'a', 'b', 'c']
+
+    model = Model(vocab_list=vocab_list)
+    # 贪婪解码
+    result_transcript = model.decode_batch_greedy(probs_split=probs_split)
+    # 束搜索解码
+    scorer = Scorer(
+        alpha=1.0,
+        beta=0.3,
+        language_model_path='./lm/zh_giga.no_cna_cmn.prune01244.klm',
+        vocab_list=model.vocab_list)
+    result_transcript = model.decode_batch_beam_search(probs_split=probs_split,
+                                                       beam_size=5,
+                                                       num_processes=16,
+                                                       scorer=scorer,
+                                                       cutoff_prob=1.0,
+                                                       cutoff_top_n=40)
