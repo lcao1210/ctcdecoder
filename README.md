e4581a86680b1d08aa6a33dace61d2a37cd1b680
lcao1210 <lcao1992@163.com>
2021-03-30 15:22:11 +0800

    20210330累积更新

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # CTC解码器
 [![Python Version](https://img.shields.io/badge/Python-3.6.9-blue.svg)](https://www.python.org/)
 
+## 功能介绍
+用于CTC解码，支持贪婪解码(greedy decode)以及束搜索解码(beam search decode)，使用方法详见`demo.py`
+
 ## 执行流程
 利用setup.sh脚本安装swig_decoders包
 ```
@@ -9,8 +12,58 @@ chmod u+x setup.sh
 ./setup.sh
 ```
 
-## 功能介绍
-用于CTC解码，支持贪婪解码(greedy decode)以及束搜索解码(beam search decode)，使用方法详见`demo.py`
+## KenLM训练
+### 已有公开模型：
+可以使用已经训练的公开KenLM模型，约2.8G
+```
+mkdir lm
+cd lm
+wget https://deepspeech.bj.bcebos.com/zh_lm/zh_giga.no_cna_cmn.prune01244.klm
+```
+
+对于有足够大性能的机器（内存大于70G），可以下载70G的超大语言模型
+```
+mkdir lm
+cd lm
+wget https://deepspeech.bj.bcebos.com/zh_lm/zhidao_giga.klm
+```
+### 自行训练：
+除了使用已有公开语言模型，还可以将自己收集到的语料进行训练
+1. 首先下载KenLM包：
+```
+wget -O - https://kheafield.com/code/kenlm.tar.gz |tar xz
+mkdir kenlm/build
+cd kenlm/build
+cmake ..
+make -j2
+```
+如果在cmake中boost部分报错，执行：
+```
+sudo apt-get install libboost-all-dev
+```
+
+2. 进入build/目录，执行操作：
+```
+bin/lmplz -o 3 --verbose header --text test.txt --arpa test.arpa
+```
+其中：
+```
+-o 为必须的参数，3表示3-gram
+–verbose header表示向ARPA文件开头添加详细的头文件（可选）
+–text 训练的语料库，每个句子一行，每个字或词用空格隔开
+–arpa 训练生成的语言模型，在这之后为了减少空间可以将其变成二进制格式
+```
+
+3. 为了节省磁盘存储空间，将 arpa 文件转换为trie二进制文件
+```
+bin/build_binary trie -a 22 -q 8 -b 8 test.arpa test.klm
+```
+其中：
+```
+-a 表示在“trie”中用于切分的指针的最高位数。
+-q 表示概率
+-b 表示退避的量化参数
+```
 
 ## 感谢
-https://github.com/yeyupiaoling/PaddlePaddle-DeepSpeech/commit/d0ebd704b5b2599e269d1f9852a489ef928e468d
+https://github.com/yeyupiaoling/PaddlePaddle-DeepSpeech/tree/d0ebd704b5b2599e269d1f9852a489ef928e468d
--- demo.py
+++ demo.py
@@ -1,108 +1,22 @@
-import swig_decoders
-
-
-def ctc_greedy_decoder(probs_seq, vocabulary):
-    """Wrapper for ctc best path decoder in swig.
-
-    :param probs_seq: 2-D list of probability distributions over each time
-                      step, with each element being a list of normalized
-                      probabilities over vocabulary and blank.
-    :type probs_seq: 2-D list
-    :param vocabulary: Vocabulary list.
-    :type vocabulary: list
-    :return: Decoding result string.
-    :rtype: str
-    """
-    result = swig_decoders.ctc_greedy_decoder(probs_seq.tolist(), vocabulary)
-    return result
-
-
-def ctc_beam_search_decoder(probs_seq,
-                            vocabulary,
-                            beam_size,
-                            cutoff_prob=1.0,
-                            cutoff_top_n=40,
-                            ext_scoring_func=None):
-    """Wrapper for the CTC Beam Search Decoder.
-
-    :param probs_seq: 2-D list of probability distributions over each time
-                      step, with each element being a list of normalized
-                      probabilities over vocabulary and blank.
-    :type probs_seq: 2-D list
-    :param vocabulary: Vocabulary list.
-    :type vocabulary: list
-    :param beam_size: Width for beam search.
-    :type beam_size: int
-    :param cutoff_prob: Cutoff probability in pruning,
-                        default 1.0, no pruning.
-    :type cutoff_prob: float
-    :param cutoff_top_n: Cutoff number in pruning, only top cutoff_top_n
-                         characters with highest probs in vocabulary will be
-                         used in beam search, default 40.
-    :type cutoff_top_n: int
-    :param ext_scoring_func: External scoring function for
-                             partially decoded sentence, e.g. word count
-                             or language model.
-    :type external_scoring_func: callable
-    :return: List of tuples of log probability and sentence as decoding
-             results, in descending order of the probability.
-    :rtype: list
-    """
-    beam_results = swig_decoders.ctc_beam_search_decoder(
-        probs_seq.tolist(), vocabulary, beam_size, cutoff_prob, cutoff_top_n,
-        ext_scoring_func)
-    beam_results = [(res[0], res[1]) for res in beam_results]
-    return beam_results
-
-
-def ctc_beam_search_decoder_batch(probs_split,
-                                  vocabulary,
-                                  beam_size,
-                                  num_processes,
-                                  cutoff_prob=1.0,
-                                  cutoff_top_n=40,
-                                  ext_scoring_func=None):
-    """Wrapper for the batched CTC beam search decoder.
-
-    :param probs_seq: 3-D list with each element as an instance of 2-D list
-                      of probabilities used by ctc_beam_search_decoder().
-    :type probs_seq: 3-D list
-    :param vocabulary: Vocabulary list.
-    :type vocabulary: list
-    :param beam_size: Width for beam search.
-    :type beam_size: int
-    :param num_processes: Number of parallel processes.
-    :type num_processes: int
-    :param cutoff_prob: Cutoff probability in vocabulary pruning,
-                        default 1.0, no pruning.
-    :type cutoff_prob: float
-    :param cutoff_top_n: Cutoff number in pruning, only top cutoff_top_n
-                         characters with highest probs in vocabulary will be
-                         used in beam search, default 40.
-    :type cutoff_top_n: int
-    :param num_processes: Number of parallel processes.
-    :type num_processes: int
-    :param ext_scoring_func: External scoring function for
-                             partially decoded sentence, e.g. word count
-                             or language model.
-    :type external_scoring_function: callable
-    :return: List of tuples of log probability and sentence as decoding
-             results, in descending order of the probability.
-    :rtype: list
-    """
-    probs_split = [probs_seq.tolist() for probs_seq in probs_split]
-
-    batch_beam_results = swig_decoders.ctc_beam_search_decoder_batch(
-        probs_split, vocabulary, beam_size, num_processes, cutoff_prob,
-        cutoff_top_n, ext_scoring_func)
-    batch_beam_results = [[(res[0], res[1]) for res in beam_results]
-                          for beam_results in batch_beam_results]
-    return batch_beam_results
+from decoders.ctcdecoders.swig_wrapper import Scorer
+from decoders.ctcdecoders.swig_wrapper import ctc_beam_search_decoder_batch
+from decoders.ctcdecoders.swig_wrapper import ctc_greedy_decoder
 
 
 class Model():
-    def __init__(self):
-        pass
+    def __init__(self, alpha, beta, language_model_path, vocab_list,
+                 batch_size, cutoff_prob, cutoff_top_n):
+        self.alpha = alpha
+        self.beta = beta
+        self.language_model_path = language_model_path
+        self.vocab_list = vocab_list
+        self.batch_size = batch_size
+        self.cutoff_prob = cutoff_prob
+        self.cutoff_top_n = cutoff_top_n
+        self.scorer = Scorer(alpha=self.alpha,
+                             beta=self.beta,
+                             language_model_path=self.language_model_path,
+                             vocab_list=self.vocab_list)
 
     def decode_batch_greedy(self, probs_split, vocab_list):
         results = []
@@ -115,7 +29,6 @@ class Model():
     def decode_batch_beam_search(self, probs_split, beam_alpha, beam_beta,
                                  beam_size, cutoff_prob, cutoff_top_n,
                                  vocab_list, num_processes):
-        # beam search decode
         num_processes = min(num_processes, len(probs_split))
         beam_search_results = ctc_beam_search_decoder_batch(
             probs_split=probs_split,
@@ -130,14 +43,17 @@ class Model():
         return results
 
 
-model = Model()
-result_transcript = model.decode_batch_greedy(probs_split=probs_split,
-                                              vocab_list=vocab_list)
-result_transcript = model.decode_batch_beam_search(probs_split=probs_split,
-                                                   beam_alpha=alpha,
-                                                   beam_beta=beta,
-                                                   beam_size=beam_size,
-                                                   cutoff_prob=cutoff_prob,
-                                                   cutoff_top_n=cutoff_top_n,
-                                                   vocab_list=vocab_list,
-                                                   num_processes=16)
+if __name__ == '__main__':
+    model = Model()
+    probs_split = [[0.6, 0.4], [0.2, 0.8], [0.3, 0.7]]
+    result_transcript = model.decode_batch_greedy(probs_split=probs_split,
+                                                  vocab_list=model.vocab_list)
+    result_transcript = model.decode_batch_beam_search(
+        probs_split=probs_split,
+        beam_alpha=model.alpha,
+        beam_beta=model.beta,
+        beam_size=model.beam_size,
+        cutoff_prob=model.cutoff_prob,
+        cutoff_top_n=model.cutoff_top_n,
+        vocab_list=model.vocab_list,
+        num_processes=16)
