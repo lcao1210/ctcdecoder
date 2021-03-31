7936eff8c0be971b7ac52e5e3d9a03839b787232
lcao1210 <lcao1992@163.com>
2021-03-31 16:50:38 +0800

    20210331累积更新

--- README.md
+++ README.md
@@ -2,10 +2,10 @@
 [![Python Version](https://img.shields.io/badge/Python-3-blue.svg)](https://www.python.org/)
 
 ## 功能介绍
-用于CTC解码，支持贪婪解码(greedy decode)以及束搜索解码(beam search decode)，使用方法详见`demo.py`
+用于CTC解码，支持贪婪解码(greedy decode)以及束搜索解码(beam search decode)
 
 ## 执行流程
-利用setup.sh脚本安装swig_decoders包
+利用setup.sh脚本安装swig_decoders包，安装过程如下，安装完成后即可直接调用`./decoders/ctcsecoder/swig_wrapper.py`中的相关贪婪解码函数以及束搜索解码函数，调用方法参见`demo.py`
 ```
 cd decoder
 chmod u+x setup.sh
@@ -13,6 +13,7 @@ chmod u+x setup.sh
 ```
 
 ## KenLM训练
+对于束搜索解码，需要向解码器中添加KenLM语言模型，可以通过下载已有公开模型或自行训练的方式获得：
 ### 已有公开模型：
 可以使用已经训练的公开KenLM模型，约2.8G
 ```
@@ -50,7 +51,7 @@ bin/lmplz -o 3 --verbose header --text test.txt --arpa test.arpa
 ```
 -o 为必须的参数，3表示3-gram
 –verbose header表示向ARPA文件开头添加详细的头文件（可选）
-–text 训练的语料库，每个句子一行，每个字或词用空格隔开
+–text 训练的语料库，每个句子一行，每个字或词用空格隔开，最好不要包含标点符号以及阿拉伯数字
 –arpa 训练生成的语言模型，在这之后为了减少空间可以将其变成二进制格式
 ```
 
@@ -60,7 +61,7 @@ bin/build_binary trie -a 22 -q 8 -b 8 test.arpa test.klm
 ```
 其中：
 ```
--a 表示在“trie”中用于切分的指针的最高位数。
+-a 表示在trie中用于切分的指针的最高位数。
 -q 表示概率
 -b 表示退避的量化参数
 ```
--- demo.py
+++ demo.py
@@ -1,53 +1,55 @@
-from decoders.ctcdecoders.swig_wrapper import Scorer
-from decoders.ctcdecoders.swig_wrapper import ctc_beam_search_decoder_batch
-from decoders.ctcdecoders.swig_wrapper import ctc_greedy_decoder
+import numpy as np
+
+from decoders.ctcdecoders.swig_wrapper import (Scorer,
+                                               ctc_beam_search_decoder_batch,
+                                               ctc_greedy_decoder)
 
 
-class Model():
-    def __init__(self, vocab_list):
-        self.vocab_list = vocab_list  # 字符集
+def decode_batch_greedy(probs_split, vocab_list):
+    # 批量贪婪解码，参数含义参见./decoders/ctcdecoders/swig_wrapper.py
+    results = []
+    for i, probs in enumerate(probs_split):
+        output_transcription = ctc_greedy_decoder(probs_seq=probs,
+                                                  vocabulary=vocab_list)
+        results.append(output_transcription)
+    return results
 
-    def decode_batch_greedy(self, probs_split):
-        # 参数含义参见./decoders/ctcdecoders/swig_wrapper.py
-        results = []
-        for i, probs in enumerate(probs_split):
-            output_transcription = ctc_greedy_decoder(
-                probs_seq=probs, vocabulary=self.vocab_list)
-            results.append(output_transcription)
-        return results
 
-    def decode_batch_beam_search(self, probs_split, beam_size, num_processes,
-                                 scorer, cutoff_prob, cutoff_top_n):
-        # 参数含义参见./decoders/ctcdecoders/swig_wrapper.py
-        num_processes = min(num_processes, len(probs_split))
-        beam_search_results = ctc_beam_search_decoder_batch(
-            probs_split=probs_split,
-            vocabulary=self.vocab_list,
-            beam_size=beam_size,
-            num_processes=num_processes,
-            ext_scoring_func=scorer,
-            cutoff_prob=cutoff_prob,
-            cutoff_top_n=cutoff_top_n)
-        results = [result[0][1] for result in beam_search_results]
-        return results
+def decode_batch_beam_search(probs_split, vocab_list, beam_size, num_processes,
+                             scorer, cutoff_prob, cutoff_top_n):
+    # 批量束搜索解码，参数含义参见./decoders/ctcdecoders/swig_wrapper.py
+    num_processes = min(num_processes, len(probs_split))
+    beam_search_results = ctc_beam_search_decoder_batch(
+        probs_split=probs_split,
+        vocabulary=vocab_list,
+        beam_size=beam_size,
+        num_processes=num_processes,
+        ext_scoring_func=scorer,
+        cutoff_prob=cutoff_prob,
+        cutoff_top_n=cutoff_top_n)
+    results = [result[0][1] for result in beam_search_results]
+    return results
 
 
 if __name__ == '__main__':
-    probs_split = [[0.6, 0.4], [0.2, 0.8], [0.3, 0.7]]
+    # probs_split为一个list，每个元素shape为(seq_len，label_num)，且每个时间步经过归一化
+    probs_split = [np.array([[0.6, 0.4], [0.2, 0.8], [0.3, 0.7]])]
+    # 每个元素的索引号要与训练时标签对应的索引号一致
     vocab_list = ['_', 'a', 'b', 'c']
 
-    model = Model(vocab_list=vocab_list)
-    # 贪婪解码
-    result_transcript = model.decode_batch_greedy(probs_split=probs_split)
-    # 束搜索解码
+    # 批量贪婪解码
+    result_transcript = decode_batch_greedy(probs_split=probs_split,
+                                            vocab_list=vocab_list)
+    # 批量束搜索解码
     scorer = Scorer(
         alpha=1.0,
         beta=0.3,
         language_model_path='./lm/zh_giga.no_cna_cmn.prune01244.klm',
-        vocab_list=model.vocab_list)
-    result_transcript = model.decode_batch_beam_search(probs_split=probs_split,
-                                                       beam_size=5,
-                                                       num_processes=16,
-                                                       scorer=scorer,
-                                                       cutoff_prob=1.0,
-                                                       cutoff_top_n=40)
+        vocab_list=vocab_list)
+    result_transcript = decode_batch_beam_search(probs_split=probs_split,
+                                                 vocab_list=vocab_list,
+                                                 beam_size=5,
+                                                 num_processes=16,
+                                                 scorer=scorer,
+                                                 cutoff_prob=1.0,
+                                                 cutoff_top_n=40)
